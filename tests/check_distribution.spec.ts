6f66d7476dd73609a72ef08ae31467921c6af827
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2022-10-28 14:51:37 +0900

    chore: break down test cases

--- tests/check_distribution.spec.ts
+++ tests/check_distribution.spec.ts
@@ -1,28 +1,28 @@
-import { promises as fs } from "fs";
+import * as fs from "fs";
 import * as path from "path";
 import * as jszip from "jszip";
 
 const templateDir = path.join(__dirname, "..", "dist");
 
+const templateZipPaths = fs.readdirSync(templateDir, { withFileTypes: true })
+	.filter(dirent => dirent.isFile() && path.extname(dirent.name) === ".zip")
+	.map(dirent => path.join(templateDir, dirent.name));
+
+if (!templateZipPaths || !templateZipPaths.length) {
+	throw new Error("distribution does not exist.");
+}
+
 describe("check distribution", () => {
-	it("check the directory structure of the distribution", async () => {
-		const templateZipPaths = (await fs.readdir(templateDir, { withFileTypes: true }))
-			.filter(dirent => dirent.isFile() && path.extname(dirent.name) === ".zip")
-			.map(dirent => path.join(templateDir, dirent.name));
+	for (let templateZipPath of templateZipPaths) {
+		const name = path.basename(templateZipPath, path.extname(templateZipPath)); // /path/to/javascript.zip -> javascript
 
-		if (!templateZipPaths || !templateZipPaths.length) {
-			throw new Error("distribution does not exist.");
-		}
-
-		for (let templateZipPath of templateZipPaths) {
-			const data = await fs.readFile(templateZipPath);
+		it(`check the directory structure of the "${name}"`, async () => {
+			const data = fs.readFileSync(templateZipPath);
 			const zip = await jszip.loadAsync(data);
-			const name = path.basename(templateZipPath, path.extname(templateZipPath)); // /path/to/javascript.zip -> javascript
-
 			// root にテンプレート名のディレクトリが存在することを確認
 			expect(zip.files[name + "/"]).not.toBeUndefined();
 			// テンプレート名のディレクトリ直下に game.json が存在することを確認
 			expect(zip.files[name + "/game.json"]).not.toBeUndefined();
-		}
-	});
+		});
+	}
 });
