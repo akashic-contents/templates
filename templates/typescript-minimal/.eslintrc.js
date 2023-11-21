daf5c65b2e26d9c23a1ca37b6a34057b2c4d095f
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2023-11-21 14:21:44 +0900

    Mod .eslintrc.js

--- templates/typescript-minimal/.eslintrc.js
+++ templates/typescript-minimal/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-    "project": "tsconfig.json",
+    "project": ["tsconfig.json", "tsconfig.jest.json"],
     "sourceType": "module"
   },
   "plugins": [
