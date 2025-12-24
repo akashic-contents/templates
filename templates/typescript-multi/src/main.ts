3997c80a3238045f3ad47ea01700baab9324197b
ShinobuTakahashi <a.little.more.than.before@gmail.com>
2025-12-24 11:18:28 +0900

    Mod typescript-multi

--- templates/typescript-multi/src/main.ts
+++ templates/typescript-multi/src/main.ts
@@ -132,7 +132,7 @@ export function main(param: GameMainParameterObject): void {
 				// 移動先との差分を計算します
 				const dx = forwardTo.x - character.x;
 				const dy = forwardTo.y - character.y;
-				const dist = Math.sqrt(dx * dx + dy * dy);
+				const dist = Math.sqrt(dx * dx + dy * dy); // eslint-disable-line akashic/warn-global-math
 
 				if (dist > speed) {
 					// 一定速度で近づきます
